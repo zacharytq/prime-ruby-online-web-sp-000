b1069a3cb372074599308a3e7279ebcd85bcf45c
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 14:29:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num <=1
+    return false
+  elsif num <= 3
+    return true
+  elsif num % 2 == 0 || num % 3 == 0
+    return false
+  end
+
+  i = 5
+
+  while i * i <= num
+    if num % i == 0
+      return false
+    else
+      i += 6
+      puts i
+    end
+  end
+  true
+end
