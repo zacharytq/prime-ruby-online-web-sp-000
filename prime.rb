8a2d41e6e36b5676243ba180bb1390aed92c1974
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 14:17:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(num)
+  if num <=1
+    false
+  elsif num <= 3
+    true
+  elsif num % 2 == 0 || num % 3 == 0
+    false
+  else
+    i = 5
+    w = 2
+
+    while i * i <= num
+      if num % i == 0
+        false
+      else
+        i += w
+        w = 6 - w
+      end
+    end
+    true
+  end
+end
