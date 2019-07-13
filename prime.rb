5308d61466f0a8ba9e6b02e83304aa6aad276523
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 14:29:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def prime?(num)
   if num <=1
-    false
+    return false
   elsif num <= 3
-    true
+    return true
   elsif num % 2 == 0 || num % 3 == 0
-    false
+    return false
   end
 
   i = 5
